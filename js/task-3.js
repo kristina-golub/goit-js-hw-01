5e7b48439fe3781f4ba7c0b1fb0d1077fe8dea2b
Kristina Golub <kristigolub26@gmail.com>
2024-11-21 15:52:19 +0100

    hw1

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(contentNpx, paddingNpx, borderNpx) {
+    const content = Number.parseFloat(contentNpx);
+    const padding = Number.parseFloat(paddingNpx);
+    const border = Number.parseFloat(borderNpx);
+    return `${content + padding * 2 + border * 2}`;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
